27799552651d4f2a6def19637ecf44d624a0031b
Gjermund Garaba <gjermund@garaba.net>
2023-03-13 14:36:36 +0100

    Added preliminery evidence checker

--- gon-cli/cmd/gon_validate_evidence.go
+++ gon-cli/cmd/gon_validate_evidence.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/gjermundgaraba/gon/chains"
 	"github.com/xuri/excelize/v2"
 	"path"
+	"strconv"
 	"strings"
 )
 
@@ -25,8 +27,51 @@ func validateEvidenceFileInteractive() {
 		}
 	}()
 
-	validateInfoSheet(evidence)
+	errors := validateEvidenceFile(evidence)
+	errorsFound := false
+	for sheet, sheetErrors := range errors {
+		if len(sheetErrors) > 0 {
+			errorsFound = true
+			fmt.Println()
+			fmt.Println("Errors in sheet", sheet)
+			for _, sheetError := range sheetErrors {
+				fmt.Println(sheetError)
+			}
+		}
+	}
 
+	if !errorsFound {
+		fmt.Println("Congratulations! Your evidence file appears to be valid!")
+		fmt.Println("Keep in mind, not all fields are fully validated here yet, so please double check your evidence file.")
+	}
+}
+
+func validateEvidenceFile(evidence *excelize.File) (validationErrors map[string][]string) {
+	validationErrors = make(map[string][]string)
+
+	validationErrors["Info"] = validateInfoSheet(evidence)
+	validationErrors["A1"] = validateA1Sheet(evidence)
+	validationErrors["A2"] = validateA2Sheet(evidence)
+	validationErrors["A3"] = validateA3Sheet(evidence)
+	validationErrors["A4"] = validateA4Sheet(evidence)
+	validationErrors["A5"] = validateA5Sheet(evidence)
+	validationErrors["A6"] = validateA6Sheet(evidence)
+	validationErrors["A7"] = validateA7Sheet(evidence)
+	validationErrors["A8"] = validateA8Sheet(evidence)
+	validationErrors["A9"] = validateA9Sheet(evidence)
+	validationErrors["A10"] = validateA10Sheet(evidence)
+	validationErrors["A11"] = validateA11Sheet(evidence)
+	validationErrors["A12"] = validateA12Sheet(evidence)
+	validationErrors["A13"] = validateA13Sheet(evidence)
+	validationErrors["A14"] = validateA14Sheet(evidence)
+	validationErrors["A15"] = validateA15Sheet(evidence)
+	validationErrors["A16"] = validateA16Sheet(evidence)
+	validationErrors["A17"] = validateA17Sheet(evidence)
+	validationErrors["A18"] = validateA18Sheet(evidence)
+	validationErrors["A19"] = validateA19Sheet(evidence)
+	validationErrors["A20"] = validateA20Sheet(evidence)
+
+	return validationErrors
 }
 
 // ## Info
@@ -35,13 +80,287 @@ func validateEvidenceFileInteractive() {
 // - `row length` == `2` (some merge the row  2 and row 3, I get confused about this...)
 // - input only in `row 2`
 func validateInfoSheet(evidence *excelize.File) (validationErrors []string) {
-	infoRows, err := evidence.GetRows("Info")
-	if err != nil {
-		return []string{"Info sheet not found"}
+	validationErrors = validateSheet(evidence, "Info", 2, []string{"TeamName", "IRISnetAddress", "StargazeAddress", "JunoAddress", "UptickAddress", "OmniFlixAddress", "DiscordHandle", "Community"}, false)
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
+	infoRows, _ := evidence.GetRows("Info")
+
+	for i, _ := range make([]int, 7) {
+		if infoRows[1][i] == "" {
+			validationErrors = append(validationErrors, fmt.Sprintf("Column %q, should not be empty", infoRows[0][i]))
+		}
 	}
 
-	if len(infoRows) != 2 {
-		validationErrors = append(validationErrors, "Info sheet should have 2 rows exactly")
+	return validationErrors
+}
+
+// ## A1
+//
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA1Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A1", 2, []string{"TxHash", "ClassID"}, true)
+}
+
+// ## A2
+//
+// - `sheet name` is unmodified
+// - `row length` >= `3`
+// - input from `row 2` (but we check only the first 2 NFTs)
+func validateA2Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A2", 3, []string{"TxHash", "ClassID", "NFTID"}, true)
+}
+
+// ## A3
+//
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA3Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A3", 2, []string{"TxHash", "ClassID", "NFTID", "ChainID"}, true)
+}
+
+// ## A4
+//
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA4Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A4", 2, []string{"TxHash", "ClassID", "NFTID", "ChainID"}, true)
+}
+
+// ## A5
+//
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA5Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A5", 2, []string{"TxHash", "ClassID", "NFTID", "ChainID"}, true)
+}
+
+// ## A6
+//
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA6Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A6", 2, []string{"TxHash", "ClassID", "NFTID", "ChainID"}, true)
+}
+
+// ## A7
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA7Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A7", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A8
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA8Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A8", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A9
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA9Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A9", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A10
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA10Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A10", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A11
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA11Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A11", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A12
+//
+// for each of these tasks:
+// - `sheet name` is unmodified
+// - `row length` == `2`
+// - input only in `row 2`
+func validateA12Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheet(evidence, "A12", 2, []string{"ClassID", "NFTID"}, true)
+}
+
+// ## A13
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA13Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A13", []string{"TxHash", "ChainID"}, true, "i --(1)--> s --(1)--> u --(1)--> s --(2)--> i")
+}
+
+// ## A14
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA14Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A14", []string{"TxHash", "ChainID"}, true, "i --(1)--> u --(1)--> o --(1)--> u --(2)--> i")
+}
+
+// ## A15
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA15Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A15", []string{"TxHash", "ChainID"}, true, "i --(1)--> j --(1)--> u --(1)--> j --(2)--> i")
+}
+
+// ## A16
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA16Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A16", []string{"TxHash", "ChainID"}, true, "i --(1)--> j --(1)--> s --(1)--> j --(2)--> i")
+}
+
+// ## A17
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA17Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A17", []string{"TxHash", "ChainID"}, true, "i --(1)--> s --(1)--> j --(1)--> s --(1)--> i")
+}
+
+// ## A18
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA18Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A18", []string{"TxHash", "ChainID"}, true, "i --(1)--> o --(1)--> u --(1)--> o --(1)--> i")
+}
+
+// ## A19
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA19Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A19", []string{"TxHash", "ChainID"}, true, "i --(1)--> s --(1)--> j --(1)--> u --(1)--> j --(1)--> s --(1)--> i")
+}
+
+// ## A20
+//
+// - `sheet name` is unmodified
+// - `row length` == `max-hop + 1`  (for example: in `i -> s -> j -> i`, `max-hop` is `3` and `row length` must be `4`)
+// - input from `row 2`
+func validateA20Sheet(evidence *excelize.File) (validationErrors []string) {
+	return validateSheetWithFlow13to20(evidence, "A20", []string{"TxHash", "ChainID"}, true, "i --(1)--> u --(1)--> s --(1)--> o --(1)--> s --(1)--> u --(1)--> i")
+}
+
+func validateSheetWithFlow13to20(evidence *excelize.File, sheetName string, expectedHeaders []string, allFieldsMandatory bool, flow string) (validationErrors []string) {
+	connections := parseFlow(flow)
+	expectedNumberOfRows := len(connections) + 1
+	validationErrors = validateSheet(evidence, sheetName, expectedNumberOfRows, expectedHeaders, allFieldsMandatory)
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
+
+	// check if the flow is correct
+	for i, connection := range connections {
+		value, err := evidence.GetCellValue(sheetName, fmt.Sprintf("B%d", i+2))
+		if err != nil {
+			panic(err)
+		}
+
+		if value != string(connection.ChannelA.ChainID) {
+			validationErrors = append(validationErrors, fmt.Sprintf("ChainID for the row %d should be %q, but was %q", i+2, connection.ChannelA.ChainID, value))
+		}
+	}
+
+	return validationErrors
+}
+
+var chainMap = map[string]chains.Chain{
+	"i": chains.IRISChain,
+	"s": chains.StargazeChain,
+	"j": chains.JunoChain,
+	"o": chains.OmniFlixChain,
+	"u": chains.UptickChain,
+}
+
+// example: i --(1)--> s --(1)--> j --(1)--> i
+func parseFlow(flow string) []chains.NFTConnection {
+	var connections []chains.NFTConnection
+	flowPieces := strings.Split(flow, ")-->")
+	for i, fp := range flowPieces {
+		if i != len(flowPieces)-1 {
+			chain := chainMap[string(strings.TrimSpace(fp)[0])]
+			nextChain := chainMap[string(strings.TrimSpace(flowPieces[i+1])[0])]
+			connectionNumber, err := strconv.ParseInt(string(fp[len(fp)-1]), 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			connections = append(connections, chain.GetConnectionsTo(nextChain)[connectionNumber-1])
+		}
+	}
+
+	return connections
+}
+
+func validateSheet(evidence *excelize.File, sheetName string, expectedNumberOfRows int, expectedHeaders []string, allFieldsMandatory bool) (validationErrors []string) {
+	rows, err := evidence.GetRows(sheetName)
+	if err != nil {
+		return []string{fmt.Sprintf("%s sheet not found", sheetName)}
+	}
+
+	if len(rows) != expectedNumberOfRows {
+		return append(validationErrors, fmt.Sprintf("%s sheet should have %d rows exactly, but has %d rows", sheetName, expectedNumberOfRows, len(rows)))
+	}
+
+	for i, header := range expectedHeaders {
+		actualHeader := rows[0][i]
+		if actualHeader != header {
+			validationErrors = append(validationErrors, fmt.Sprintf("Sheet headers should not be changed, expected %s, got %s", header, actualHeader))
+		}
+	}
+
+	if allFieldsMandatory {
+		// Skip the first row, which is the header
+		for i := 1; i < expectedNumberOfRows; i++ {
+			if len(rows[i]) != len(expectedHeaders) {
+				validationErrors = append(validationErrors, fmt.Sprintf("All fields are mandatory, but row %d has %d column(s), expected %d", i+1, len(rows[i]), len(expectedHeaders)))
+				continue
+			}
+			for j, cell := range rows[i] {
+				if cell == "" {
+					validationErrors = append(validationErrors, fmt.Sprintf("All fields are mandatory, but %s is empty", expectedHeaders[j]))
+				}
+			}
+		}
 	}
 
 	return validationErrors
---
+++ gon-cli/cmd/gon_validate_evidence_test.go
@@ -0,0 +1,273 @@
+package cmd
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/xuri/excelize/v2"
+	"testing"
+)
+
+func TestValidateEvidence(t *testing.T) {
+	evidence, err := excelize.OpenFile("testdata/evidence.xlsx")
+	require.NoError(t, err)
+
+	validationErrors := validateEvidenceFile(evidence)
+	require.Equal(t, 0, len(validationErrors))
+}
+
+func createEmptySheetWithHeaders(evidence *excelize.File, sheet string, headers []string) {
+	if _, err := evidence.NewSheet(sheet); err != nil {
+		panic(err)
+	}
+
+	if err := evidence.SetSheetRow(sheet, "A1", &headers); err != nil {
+		panic(err)
+	}
+}
+
+func createEmptyInfoSheetWithHeaders(evidence *excelize.File) {
+	createEmptySheetWithHeaders(evidence, "Info", []string{"TeamName", "IRISnetAddress", "StargazeAddress", "JunoAddress", "UptickAddress", "OmniFlixAddress", "DiscordHandle", "Community"})
+}
+
+func fillRowWithJunk(evidence *excelize.File, sheet, startingCell string, numberOfValues int) {
+	row := make([]interface{}, numberOfValues)
+	for i, _ := range row {
+		row[i] = "test"
+	}
+
+	if err := evidence.SetSheetRow(sheet, startingCell, &row); err != nil {
+		panic(err)
+	}
+}
+
+func TestValidateInfoSheet(t *testing.T) {
+	testTable := map[string]struct {
+		setupTestEvidence func(evidence *excelize.File)
+		expectedErrors    []string
+	}{
+		"Info sheet not found": {
+			setupTestEvidence: func(evidence *excelize.File) {},
+			expectedErrors:    []string{"Info sheet not found"},
+		},
+		"Info sheet should have 2 rows exactly": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptyInfoSheetWithHeaders(evidence)
+				fillRowWithJunk(evidence, "Info", "A2", 8)
+				fillRowWithJunk(evidence, "Info", "A3", 8)
+			},
+			expectedErrors: []string{"Info sheet should have 2 rows exactly, but has 3 rows"},
+		},
+		"Info sheet headers should not be changed": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				if _, err := evidence.NewSheet("Info"); err != nil {
+					panic(err)
+				}
+				fillRowWithJunk(evidence, "Info", "A1", 8)
+				fillRowWithJunk(evidence, "Info", "A2", 8)
+			},
+			expectedErrors: []string{
+				"Sheet headers should not be changed, expected TeamName, got test",
+				"Sheet headers should not be changed, expected IRISnetAddress, got test",
+				"Sheet headers should not be changed, expected StargazeAddress, got test",
+				"Sheet headers should not be changed, expected JunoAddress, got test",
+				"Sheet headers should not be changed, expected UptickAddress, got test",
+				"Sheet headers should not be changed, expected OmniFlixAddress, got test",
+				"Sheet headers should not be changed, expected DiscordHandle, got test",
+				"Sheet headers should not be changed, expected Community, got test"},
+		},
+		"Mandatory fields should not be empty": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptyInfoSheetWithHeaders(evidence)
+				if err := evidence.SetCellStr("Info", "H2", "This one nobody cares about, the rest are mandatory"); err != nil {
+					panic(err)
+				}
+			},
+			expectedErrors: []string{
+				"Column \"TeamName\", should not be empty",
+				"Column \"IRISnetAddress\", should not be empty",
+				"Column \"StargazeAddress\", should not be empty",
+				"Column \"JunoAddress\", should not be empty",
+				"Column \"UptickAddress\", should not be empty",
+				"Column \"OmniFlixAddress\", should not be empty",
+				"Column \"DiscordHandle\", should not be empty",
+			},
+		},
+	}
+
+	for name, test := range testTable {
+		t.Run(name, func(t *testing.T) {
+			evidence := excelize.NewFile()
+			test.setupTestEvidence(evidence)
+			validationErrors := validateInfoSheet(evidence)
+			require.Equal(t, test.expectedErrors, validationErrors)
+		})
+	}
+}
+
+func TestValidateA1(t *testing.T) {
+	testTable := map[string]struct {
+		setupTestEvidence func(evidence *excelize.File)
+		expectedErrors    []string
+	}{
+		"A1 sheet not found": {
+			setupTestEvidence: func(evidence *excelize.File) {},
+			expectedErrors:    []string{"A1 sheet not found"},
+		},
+		"Info sheet should have 2 rows exactly": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A1", []string{"TxHash", "ClassID"})
+				fillRowWithJunk(evidence, "A1", "A2", 2)
+				fillRowWithJunk(evidence, "A1", "A3", 2)
+			},
+			expectedErrors: []string{"A1 sheet should have 2 rows exactly, but has 3 rows"},
+		},
+		"A1 sheet headers should not be changed": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				if _, err := evidence.NewSheet("A1"); err != nil {
+					panic(err)
+				}
+				fillRowWithJunk(evidence, "A1", "A1", 2)
+				fillRowWithJunk(evidence, "A1", "A2", 2)
+			},
+			expectedErrors: []string{
+				"Sheet headers should not be changed, expected TxHash, got test",
+				"Sheet headers should not be changed, expected ClassID, got test",
+			},
+		},
+		"Mandatory fields should not be empty": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A1", []string{"TxHash", "ClassID"})
+				if err := evidence.SetCellStr("A1", "A2", "mytxhash"); err != nil {
+					panic(err)
+				}
+			},
+			expectedErrors: []string{
+				"All fields are mandatory, but row 2 has 1 column(s), expected 2",
+			},
+		},
+	}
+
+	for name, test := range testTable {
+		t.Run(name, func(t *testing.T) {
+			evidence := excelize.NewFile()
+			test.setupTestEvidence(evidence)
+			validationErrors := validateA1Sheet(evidence)
+			require.Equal(t, test.expectedErrors, validationErrors)
+		})
+	}
+}
+
+func TestValidateA13(t *testing.T) {
+	testTable := map[string]struct {
+		setupTestEvidence func(evidence *excelize.File)
+		expectedErrors    []string
+	}{
+		"A13 sheet not found": {
+			setupTestEvidence: func(evidence *excelize.File) {},
+			expectedErrors:    []string{"A13 sheet not found"},
+		},
+		"Info sheet should have 2 rows exactly": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A13", []string{"TxHash", "ChainID"})
+				fillRowWithJunk(evidence, "A13", "A2", 2)
+				fillRowWithJunk(evidence, "A13", "A3", 2)
+			},
+			expectedErrors: []string{"A13 sheet should have 5 rows exactly, but has 3 rows"},
+		},
+		"A13 sheet headers should not be changed": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				if _, err := evidence.NewSheet("A13"); err != nil {
+					panic(err)
+				}
+				fillRowWithJunk(evidence, "A13", "A1", 2)
+
+				if err := evidence.SetSheetRow("A13", "A2", &[]interface{}{"mytxhash", "gon-irishub-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A3", &[]interface{}{"mytxhash", "elgafar-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A4", &[]interface{}{"mytxhash", "uptick_7000-2"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A5", &[]interface{}{"mytxhash", "elgafar-1"}); err != nil {
+					panic(err)
+				}
+			},
+			expectedErrors: []string{
+				"Sheet headers should not be changed, expected TxHash, got test",
+				"Sheet headers should not be changed, expected ChainID, got test",
+			},
+		},
+		"Mandatory fields should not be empty": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A13", []string{"TxHash", "ChainID"})
+				if err := evidence.SetCellStr("A13", "A2", "mytxhash"); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetCellStr("A13", "A3", "mytxhash"); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetCellStr("A13", "A4", "mytxhash"); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetCellStr("A13", "A5", "mytxhash"); err != nil {
+					panic(err)
+				}
+
+			},
+			expectedErrors: []string{
+				"All fields are mandatory, but row 2 has 1 column(s), expected 2",
+				"All fields are mandatory, but row 3 has 1 column(s), expected 2",
+				"All fields are mandatory, but row 4 has 1 column(s), expected 2",
+				"All fields are mandatory, but row 5 has 1 column(s), expected 2",
+			},
+		},
+		"Wrong chain id should fail": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A13", []string{"TxHash", "ChainID"})
+				if err := evidence.SetSheetRow("A13", "A2", &[]interface{}{"7C0150BFDD1F6612A22E0B4A37B328FBF1EFD18A2C60F419DF04839C9A327BD3", "gon-irishub-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A3", &[]interface{}{"3CF3465E03060588D386CC7C02B64D6E620D336D2F438A20A581413E3A2AD9B3", "Elgafar-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A4", &[]interface{}{"1F935D8F74900FAB42E434DC047357426364AE85BE2453FDA54F65459FF20F42", "uptick_7000-2"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A5", &[]interface{}{"EECDF6ADA6691F8C458721E408139826C625C31C579B989FB9E0647F011FDFED", "elgafar-1"}); err != nil {
+					panic(err)
+				}
+			},
+			expectedErrors: []string{
+				"ChainID for the row 3 should be \"elgafar-1\", but was \"Elgafar-1\"",
+			},
+		},
+		"Valid sheet should be OK": {
+			setupTestEvidence: func(evidence *excelize.File) {
+				createEmptySheetWithHeaders(evidence, "A13", []string{"TxHash", "ChainID"})
+				if err := evidence.SetSheetRow("A13", "A2", &[]interface{}{"7C0150BFDD1F6612A22E0B4A37B328FBF1EFD18A2C60F419DF04839C9A327BD3", "gon-irishub-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A3", &[]interface{}{"3CF3465E03060588D386CC7C02B64D6E620D336D2F438A20A581413E3A2AD9B3", "elgafar-1"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A4", &[]interface{}{"1F935D8F74900FAB42E434DC047357426364AE85BE2453FDA54F65459FF20F42", "uptick_7000-2"}); err != nil {
+					panic(err)
+				}
+				if err := evidence.SetSheetRow("A13", "A5", &[]interface{}{"EECDF6ADA6691F8C458721E408139826C625C31C579B989FB9E0647F011FDFED", "elgafar-1"}); err != nil {
+					panic(err)
+				}
+			},
+			expectedErrors: nil,
+		},
+	}
+
+	for name, test := range testTable {
+		t.Run(name, func(t *testing.T) {
+			evidence := excelize.NewFile()
+			test.setupTestEvidence(evidence)
+			validationErrors := validateA13Sheet(evidence)
+			require.Equal(t, test.expectedErrors, validationErrors)
+		})
+	}
+}
